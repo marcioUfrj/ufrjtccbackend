d99e0107809eceaf4a64920be332d09decf1266f
marcioUfrj <marcioteixeira23@poli.ufrj.br>
2022-06-28 20:16:05 -0300

    mudanca no index page

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json('index page from tcc_projeto/back-end')
+  res.json('index page from tcc_projeto/back-end/ MUDANDO')
 })
 
 module.exports = router
