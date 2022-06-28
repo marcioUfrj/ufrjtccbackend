922b471aa0c2847848b6c1453ad5c3d4008ba327
marcioUfrj <marcioteixeira23@poli.ufrj.br>
2022-06-28 20:24:25 -0300

    mudanca cors

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  res.json('index page from tcc_projeto/back-end/ MUDANDO')
+  res.json('index page from tcc_projeto/back-end/')
 })
 
 module.exports = router
